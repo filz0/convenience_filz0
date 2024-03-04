cb977243ae7761a829c3250cc4f4e64e2aabee5d
ZIPPY <simperzippy@gmail.com>
2024-03-04 16:12:38 +0100

    load times

--- lua/convenience/adam.lua
+++ lua/convenience/adam.lua
@@ -1,3 +1,7 @@
+CONV_LOAD_TIMES = (CONV_LOAD_TIMES && CONV_LOAD_TIMES + 1) or 1
+print("CONV loaded "..CONV_LOAD_TIMES.." time(s)!")
+
+
 -- Add client lua files
 AddCSLuaFile("sh_conv.lua")
 AddCSLuaFile("sh_misc.lua")
