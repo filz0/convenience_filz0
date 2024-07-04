7ad5437abc95b10abe14d07cda3028e041c07166
Zippy6666 <simperzippy@gmail.com>
2024-07-04 13:52:21 +0200

    init

--- lua/autorun/conv.lua
+++ lua/autorun/conv.lua
@@ -1,58 +1,5 @@
 --[[
 ==================================================================================================
-                    THE LIBRARY
-==================================================================================================
---]]
-
-
-conv = {}
-
-
---[[
-==================================================================================================
-                    INCLUDE FILES
-==================================================================================================
---]]
-
-
-local function IncludeFiles()
-
-    local files = file.Find("convenience/*", "LUA")
-
-    -- Might serve as backwards compatability...
-    AddCSLuaFile("convenience/adam.lua")
-    include("convenience/adam.lua")
-
-
-    for _, filename in ipairs(files) do
-        filepathname = "convenience/"..filename
-        if string.StartsWith(filename, "cl_") then
-
-            AddCSLuaFile(filepathname)
-
-            if CLIENT then
-                include(filepathname)
-            end
-
-        elseif string.StartsWith(filename, "sh_") then
-
-            AddCSLuaFile(filepathname)
-            include(filepathname)
-
-        elseif string.StartsWith(filename, "sv_") && SERVER then
-
-            include(filepathname)
-
-        end
-    end
-end
-
-
-IncludeFiles()
-
-
---[[
-==================================================================================================
                     MESSAGE TEMPLATE
 ==================================================================================================
 --]]
@@ -80,10 +27,75 @@ IncludeFiles()
 --         label:Dock(BOTTOM)
 --         label:SetContentAlignment(5)  -- 5 corresponds to center alignment
 --     end
--- elseif SERVER && !file.Exists("autorun/conv_adam.lua", "LUA") then
+-- elseif SERVER && !file.Exists("autorun/conv.lua", "LUA") then
 --     -- Conv lib not on on server, send message to clients
 --     hook.Add("PlayerInitialSpawn", "convenienceerrormsg", function( ply )
 --         local sendstr = 'MissingConvMsg()'
 --         ply:SendLua(sendstr)
 --     end)
 -- end
+
+
+--[[
+==================================================================================================
+                    INCLUDE FILES
+==================================================================================================
+--]]
+
+
+local rootDirectory = "convenience" -- Your directory here
+
+
+local function AddFile( File, directory )
+	local prefix = string.lower( string.Left( File, 3 ) )
+	if SERVER and prefix == "sv_" then
+
+		include( directory .. File )
+
+	elseif prefix == "sh_" then
+
+		AddCSLuaFile( directory .. File )
+		include( directory .. File )
+
+	elseif prefix == "cl_" then
+
+		if SERVER then
+			AddCSLuaFile( directory .. File )
+		elseif CLIENT then
+			include( directory .. File )
+		end
+
+	end
+end
+
+
+local function IncludeDir( directory )
+	directory = directory .. "/"
+
+	local files, directories = file.Find( directory .. "*", "LUA" )
+
+	for _, v in ipairs( files ) do
+		if string.EndsWith( v, ".lua" ) then
+			AddFile( v, directory )
+		end
+	end
+
+	for _, v in ipairs( directories ) do
+		IncludeDir( directory .. v )
+	end
+end
+
+
+IncludeDir( rootDirectory )
+
+
+--[[
+==================================================================================================
+                    CONV SPECIFICS
+==================================================================================================
+--]]
+
+
+AddCSLuaFile("convenience/adam.lua")
+include("convenience/adam.lua")
+MsgN("Zippy's library loaded!")
