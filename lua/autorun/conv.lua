62ddafa28cd7328ff9f373d00da49339fbfb0569
Zippy6666 <simperzippy@gmail.com>
2024-07-30 13:42:33 +0200

    new missing conv message

--- lua/autorun/conv.lua
+++ lua/autorun/conv.lua
@@ -1,9 +1,3 @@
---[[
-==================================================================================================
-                    CONV AUTORUN
-==================================================================================================
---]]
-
 conv = conv or {}
 
 AddCSLuaFile("conv/cl.lua")
@@ -23,39 +17,32 @@ conv.includeDir("convenience")
 
 MsgN("Zippy's library loaded!")
 
---[[
-==================================================================================================
-                    MESSAGE TEMPLATE
-==================================================================================================
---]]
 
+--[[=========================== CONV MESSAGE START ===========================]]--
+-- MissingConvMsg2 = CLIENT && function()
 
--- The message to show when CONV is not installed
--- if CLIENT then
---     function MissingConvMsg()
---         local frame = vgui.Create("DFrame")
---         frame:SetSize(300, 125)
---         frame:SetTitle("Missing Library!")
---         frame:Center()
---         frame:MakePopup()
+--     Derma_Query(
+--         "This server does not have Zippy's Library installed, addons will function incorrectly!",
 
---         local text = vgui.Create("DLabel", frame)
---         text:SetText("This server does not have the CONV library installed, some addons may function incorrectly. Click the link below to get it:")
---         text:Dock(TOP)
---         text:SetWrap(true)  -- Enable text wrapping for long messages
---         text:SetAutoStretchVertical(true)  -- Allow the text label to stretch vertically
---         text:SetFont("BudgetLabel")
+--         "ZIPPY'S LIBRARY MISSING!",
+        
+--         "Get Zippy's Library",
 
---         local label = vgui.Create("DLabelURL", frame)
---         label:SetText("CONV Library")
---         label:SetURL("https://steamcommunity.com/sharedfiles/filedetails/?id=3146473253")
---         label:Dock(BOTTOM)
---         label:SetContentAlignment(5)  -- 5 corresponds to center alignment
---     end
--- elseif SERVER && !file.Exists("autorun/conv.lua", "LUA") then
---     -- Conv lib not on on server, send message to clients
---     hook.Add("PlayerInitialSpawn", "convenienceerrormsg", function( ply )
---         local sendstr = 'MissingConvMsg()'
---         ply:SendLua(sendstr)
---     end)
--- end
+--         function()
+--             gui.OpenURL("https://steamcommunity.com/sharedfiles/filedetails/?id=3146473253")
+--         end,
+
+--         "Close"
+--     )
+
+-- end or nil
+
+-- hook.Add("PlayerInitialSpawn", "MissingConvMsg2", function( ply )
+
+--     if file.Exists("autorun/conv.lua", "LUA") then return end
+
+--     local sendstr = 'MissingConvMsg2()'
+--     ply:SendLua(sendstr)
+
+-- end)
+--[[============================ CONV MESSAGE END ============================]]--
