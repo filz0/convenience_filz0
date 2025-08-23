40d9de77c9ca2e44dba5a2fe5b4ef695effa5e14
bloodycop <89155277+bloodycop6385@users.noreply.github.com>
2025-08-23 04:29:29 +0300

    Update cl_hooks.lua

--- lua/conv/internals/cl_hooks.lua
+++ lua/conv/internals/cl_hooks.lua
@@ -2,7 +2,7 @@
 
 local function CONVScrnMSG()
 	if !table.IsEmpty( CONVScrnMSGTab ) then
- 
+
 		for id, data in pairs( CONVScrnMSGTab ) do
 
 			local text = data['Text']
@@ -21,7 +21,7 @@ local function CONVScrnMSG()
 			local startTime = data['StartTime']
 			local scrnFade = data['ScreenFade']
 
-			
+
 			local curTime = CurTime()
 			local alpha = 255
 			local alpha2 = 255
@@ -31,19 +31,19 @@ local function CONVScrnMSG()
 				alpha = fadeIn > 0 && math.Clamp( ( curTime - startTime ) / fadeIn, 0, 1 ) * 255 || 255
 
 			elseif curTime >= startTime + fadeIn + dur then
-				
+
 				alpha = fadeOut > 0 && math.Clamp( 1 - ( curTime - ( startTime + fadeIn + dur ) ) / fadeOut, 0, 1 ) * 255 || 0
 
 				if alpha <= 0 then CONVScrnMSGTab[ id ] = nil end
 
 			end
-			 
+
 			local fTColor = Color( tColor.r, tColor.g, tColor.b, alpha )
 			local fOColor = Color( OColor.r, OColor.g, OColor.b, alpha )
 
-			if startTime <= curTime then 
+			if startTime <= curTime then
 
-				draw.SimpleTextOutlined( text, font, x, y, fTColor, xAlign, yAlign, OWidth, fOColor ) 	
+				draw.SimpleTextOutlined( text, font, x, y, fTColor, xAlign, yAlign, OWidth, fOColor )
 			end
 
 		end
@@ -55,7 +55,7 @@ local cl_drawhud = GetConVar( "cl_drawhud" )
 local function CONVHUDElements()
 
 	if cl_drawhud:GetBool() then
-		
+
 		for id, data in pairs( CONVHUDElementsTab ) do
 
 			if CONVHUDElementsTab[id]['Enable'] then CONVHUDElementsTab[id]['Function']() end
@@ -68,7 +68,7 @@ end
 hook.Add("HUDPaint", "CONV", function()
 	CONVScrnMSG()
 	CONVHUDElements()
-	
+
 	CONV_HUDCurTime = CurTime()
 end)
 
@@ -82,14 +82,14 @@ hook.Add("PopulateToolMenu", "CONV", function()
 end)
 
 -- This is used to set the fog for the world, which is different from the skybox
-hook.Add("SetupWorldFog", "CONV", function() 
-	if CONV_FOG_WORLD then
+hook.Add("SetupWorldFog", "CONV", function()
+	if IsValid( CONV_FOG_WORLD ) then
 
 		render.FogStart( CONV_FOG_WORLD.FogStart )
 		render.FogEnd( CONV_FOG_WORLD.FogEnd )
 		render.FogMaxDensity( CONV_FOG_WORLD.FogMaxDensity )
 		render.FogColor( CONV_FOG_WORLD.FogColor[1], CONV_FOG_WORLD.FogColor[2], CONV_FOG_WORLD.FogColor[3] )
-		render.FogMode( CONV_FOG_WORLD.FogMode )		
+		render.FogMode( CONV_FOG_WORLD.FogMode )
 		render.SetFogZ( CONV_FOG_WORLD.FogZ )
 
 		return true
@@ -97,14 +97,14 @@ hook.Add("SetupWorldFog", "CONV", function()
 end)
 
 -- This is used to set the fog for the skybox, which is different from the world
-hook.Add("SetupSkyboxFog", "CONV", function(scale) 
-	if CONV_FOG_SKYBOX then
+hook.Add("SetupSkyboxFog", "CONV", function(scale)
+	if IsValid( CONV_FOG_SKYBOX ) then
 
 		render.FogStart( CONV_FOG_SKYBOX.FogStart * scale )
 		render.FogEnd( CONV_FOG_SKYBOX.FogEnd * scale )
 		render.FogMaxDensity( CONV_FOG_SKYBOX.FogMaxDensity )
 		render.FogColor( CONV_FOG_SKYBOX.FogColor[1], CONV_FOG_SKYBOX.FogColor[2], CONV_FOG_SKYBOX.FogColor[3] )
-		render.FogMode( CONV_FOG_SKYBOX.FogMode )		
+		render.FogMode( CONV_FOG_SKYBOX.FogMode )
 		render.SetFogZ( CONV_FOG_SKYBOX.FogZ )
 
 		return true
